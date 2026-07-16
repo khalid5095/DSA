195b9a2dc893f0ae18acedad670a09735aeb544b
Mohd Khalid <khalidnezam3@gmail.com>
2026-07-16 16:32:33 +0530

    find smallest letter greater than target

---
+++ Array/NextGreatestLetter.java
@@ -0,0 +1,22 @@
+package Backend.DSA.Array;
+
+public class NextGreatestLetter {
+    public static char nextGreatestLetter(char[] letters, char target) {
+        int start=0,end=letters.length-1;
+        while (start<=end){
+            int mid=start+(end-start)/2;
+            if (target<letters[mid]){
+                end=mid-1;
+            }
+            else {
+                start=mid+1;
+            }
+        }
+        return letters[start%letters.length];
+    }
+
+    public static void main(String[] args) {
+        char[] letters={'A','B','C','D','E','F','G','H'};
+        System.out.println(nextGreatestLetter(letters, 'C'));
+    }
+}
