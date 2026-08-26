f28697404998e1218293470a9d9404ee29b19cab
Mohd Khalid <khalidnezam3@gmail.com>
2026-08-26 22:40:45 +0530

    26-08-2026

---
+++ src/Foundation/BitExample.java
@@ -0,0 +1,10 @@
+package Foundation;
+
+public class BitExample {
+    public static void main(String[] args) {
+        int n=36;
+        int i=1;
+        int onMask=1<<i;
+        System.out.println(n | onMask);
+    }
+}
