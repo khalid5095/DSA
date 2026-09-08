16f3455f0166fe211c0294ec5a643483673bbb4a
Mohd Khalid <khalidnezam3@gmail.com>
2026-09-08 16:49:24 +0530

    08-09-26

--- src/Sorting/InsertionSort.java
+++ src/Sorting/InsertionSort.java
@@ -3,6 +3,21 @@ package Sorting;
 import java.util.Arrays;
 
 public class InsertionSort {
+    //using recursion
+    static void insertionSort1(int[] arr,int n) {
+
+            if (n<=0){
+                return;
+            }
+         insertionSort1(arr,n-1);
+            int key = arr[n-1];
+            int j = n-2;
+            while(j>=0 && arr[j]>key){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=key;
+    }
     static int[] insertionSort(int[] arr) {
         for(int i=1; i<arr.length; i++){
             int key=arr[i];
@@ -18,6 +33,7 @@ public class InsertionSort {
 
     public static void main(String[] args) {
         int [] arr={5,4,3,1,2};
-        System.out.println(Arrays.toString(insertionSort(arr)));
+        insertionSort1(arr,arr.length);
+        System.out.println(Arrays.toString(arr));
     }
 }
